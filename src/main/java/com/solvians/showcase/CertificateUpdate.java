b851971c852864bd6423ef5e5b3d4f790b9e5a9b
Mushfiq Fuad <mushfiq.fuad@gmail.com>
2025-05-07 21:07:50 +0600

    Complete CertificateUpdate Class
    Complete CertificateUpdate class with necessary parameters. Add Lombok annotations to remove boiler plate code for generating getter/setter or constructors

--- src/main/java/com/solvians/showcase/CertificateUpdate.java
+++ src/main/java/com/solvians/showcase/CertificateUpdate.java
@@ -1,6 +1,25 @@
 package com.solvians.showcase;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CertificateUpdate {
 
-    // TODO: implement me.
+    private  long timestamp;
+    private  String isin;
+    private  double bidPrice;
+    private  int bidSize;
+    private  double askPrice;
+    private  int askSize;
+    private  LocalDate maturityDate;
+
+
 }
